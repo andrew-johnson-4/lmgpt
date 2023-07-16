2f4fcc76759f2e457b7d9e555d2561994737c2c5
Andrew Johnson <andrew@subarctic.org>
2023-07-16 13:45:12 -0600

    full integration is working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use lambda_mountain::Policy;
 
+use openai_api_rust::*;
+use openai_api_rust::chat::*;
+use openai_api_rust::completions::*;
+
 fn main() {
    let mut policy = Policy::new();
    let mut lm = std::process::Command::new("lm");
@@ -8,8 +12,29 @@ fn main() {
    }
    let output = lm.output().expect("failed to execute lm process").stdout;
    let output = String::from_utf8_lossy(&output);
-   let output = output.trim();
-   if let Some((output,_)) = output.rsplit_once("\n") {
-      println!("{}", output);
+   let mut output = output.trim().to_string();
+   if let Some((o,_)) = output.rsplit_once("\n") {
+      output = o.to_string();
    }
+
+   let auth = Auth::from_env().unwrap();
+   let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
+   let body = ChatBody {
+       model: "gpt-3.5-turbo".to_string(),
+       max_tokens: None,
+       temperature: Some(0_f32),
+       top_p: Some(0_f32),
+       n: Some(2),
+       stream: Some(false),
+       stop: None,
+       presence_penalty: None,
+       frequency_penalty: None,
+       logit_bias: None,
+       user: None,
+       messages: vec![Message { role: Role::User, content: output }],
+   };
+   let rs = openai.chat_completion_create(&body);
+   let choice = rs.unwrap().choices;
+   let message = &choice[0].message.as_ref().unwrap();
+   println!("{}", message.content);
 }
