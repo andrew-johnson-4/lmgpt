34bcb98ffcbba952e4c937c45aed75198f1457c5
Andrew Johnson <andrew@subarctic.org>
2023-07-16 19:45:14 -0600

    implement random function

--- src/main.rs
+++ src/main.rs
@@ -6,25 +6,7 @@ use openai_api_rust::chat::*;
 use std::fs::File;
 use std::io::Read;
 
-fn random(args: &[Rhs]) -> Rhs {
-   println!("random");
-   Rhs::Literal("Homer".to_string())
-}
-
-fn main() {
-   let mut policy = Policy::new();
-   policy.bind_extern("random", &random);
-
-   let mut output = String::new();
-   for arg in std::env::args().skip(1) {
-      let mut p = String::new();
-      let mut file = File::open(arg).expect("load_policy: error opening file");
-      file.read_to_string(&mut p).expect("load_policy: unable to read to string");
-      if let Result::Err(o) = policy.load(&p) {
-         output = o.clone();
-      }
-   }
-
+fn chat(prompt: &str) -> String {
    let auth = Auth::from_env().unwrap();
    let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
    let body = ChatBody {
@@ -39,10 +21,38 @@ fn main() {
        frequency_penalty: None,
        logit_bias: None,
        user: None,
-       messages: vec![Message { role: Role::User, content: output }],
+       messages: vec![Message { role: Role::User, content: prompt.to_string() }],
    };
    let rs = openai.chat_completion_create(&body);
    let choice = rs.unwrap().choices;
    let message = &choice[0].message.as_ref().unwrap();
-   println!("{}", message.content);
+   return message.content.clone();
+}
+
+fn random(args: &[Rhs]) -> Rhs {
+   let mut subject = String::new();
+   for (i,a) in args.iter().enumerate() {
+      if i>0 {
+         subject.push(' ');
+      }
+      subject.push_str(&a.to_string());
+   }
+   Rhs::Literal(chat(&format!("Tell me the name of a random {}.",subject)))
+}
+
+fn main() {
+   let mut policy = Policy::new();
+   policy.bind_extern("random", &random);
+
+   let mut prompt = String::new();
+   for arg in std::env::args().skip(1) {
+      let mut p = String::new();
+      let mut file = File::open(arg).expect("load_policy: error opening file");
+      file.read_to_string(&mut p).expect("load_policy: unable to read to string");
+      if let Result::Err(o) = policy.load(&p) {
+         prompt = o.clone();
+      }
+   }
+
+   println!("{}", chat(&prompt));
 }
